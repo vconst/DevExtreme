e3bccd425e38ac86944b948c04ab138c59d6b667
volnyagin <vconst@users.noreply.github.com>
2020-11-18 11:40:51 +0300

    Remove unnecessery widgets

--- js/renovation/spike/view-extenders/group_panel_item.tsx
+++ js/renovation/spike/view-extenders/group_panel_item.tsx
@@ -1,29 +1,3 @@
-import {
-  JSXComponent, Component, ComponentBindings, OneWay, Fragment, Slot,
-} from 'devextreme-generator/component_declaration/common';
 import { createPlaceholder } from '../plugins/context';
-
-// import { isVisibleGetter, getToolbarItemsGetter } from './header_panel_getters';
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const viewFunction = ({
-  props: { column },
-}: GroupPanelItem) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Fragment>
-    { column.caption }
-  </Fragment>
-);
-
-@ComponentBindings()
-export class GroupPanelItemProps {
-  @OneWay() column: any;
-
-  @Slot() children?: any;
-}
-
-@Component({ defaultOptionRules: null, view: viewFunction })
-export class GroupPanelItem extends JSXComponent<GroupPanelItemProps>() {
-}
 
 export const GroupPanelItemPlaceholder = createPlaceholder();
